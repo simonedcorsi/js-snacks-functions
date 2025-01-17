4d65ef09613682bf4687eb02aa5ba8ee47c2bfa9
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-17 13:03:00 +0100

    conta vocali

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -1,13 +1,27 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vocali = "aeiouAEIOU"
+let contatoriVocali = 0
+let arrayVocali = []
 
 // Dichiara la funzione qui.
+function letturaStringa(stringa){
+    for( let i = 0; i < stringa.length; i++ ){
 
+        for(let k = 0; k < vocali.length; k++ ){
+            
+            if( stringa[i] === vocali[k] ){
+                contatoriVocali++
+                arrayVocali.push( vocali[k] )
+            }
+        }
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+letturaStringa(world)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log( contatoriVocali, arrayVocali )
